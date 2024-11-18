572667ae1c530007a6d1d2b35e75ebc14e52496c
marvin <imnull@outlook.com>
2024-11-18 11:30:42 +0800

    fix: null exif display

--- packages/demo/src/components/exif-list/index.tsx
+++ packages/demo/src/components/exif-list/index.tsx
@@ -7,11 +7,11 @@ export default (props: {
     const { exif } = props
     return <div className="exif-list-wrapper">
         <h3>Exif</h3>
-        <ul>{
+        {exif ? <ul>{
             Object.entries(exif).map(([key, val], idx) => <li key={idx}>
                 <label>{key}</label>
                 <span>{val}</span>
             </li>)
-        }</ul>
+        }</ul> : <h4>none</h4>}
     </div>
 }
